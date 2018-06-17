433e9b696f9c1db36e003bd6ebf53e3a40468a49
developma <developer.daisuke.maeno@gmail.com>
2018-06-17 23:50:07 +0900

    add docker file for study.

---
+++ example-postgresql/01_createtbl.sql
@@ -0,0 +1,7 @@
+create table emp (
+empid int primary key,
+empname varchar(80)
+);
+
+insert into emp (empid, empname) values (1234, 'SCOTT');
+insert into emp (empid, empname) values (5678, 'TIGER');
